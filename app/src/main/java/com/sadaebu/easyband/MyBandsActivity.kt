8a68af5cc76015c4c3b195c93092e5c3e65e1397
Jang Eunsun <20211234@duksung.ac.kr>
2024-05-07 11:53:15 +0900

    ✨ [mybands] 프래그먼트 화면 전환 기능 추가

--- app/src/main/java/com/sadaebu/easyband/MyBandsActivity.kt
+++ app/src/main/java/com/sadaebu/easyband/MyBandsActivity.kt
@@ -1,11 +1,36 @@
 package com.sadaebu.easyband
-
+import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MyBandsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_bands)
+
+        // Home Fragment를 초기 화면으로 설정
+        replaceFragment(HomeFragment())
+
+        val goHomeFragment: TextView = findViewById(R.id.mybands_home_fragment)
+        val goAlertFragment: TextView = findViewById(R.id.mybands_alert_fragment)
+
+        // Home Fragment로 이동
+        goHomeFragment.setOnClickListener {
+            replaceFragment(HomeFragment())
+        }
+
+        // Alert Fragment로 이동
+        goAlertFragment.setOnClickListener {
+            replaceFragment(AlertFragment())
+        }
+    }
+
+    // Fragment 화면 전환
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
